cd5fedafae96a204103ce2004e2cad491202ee24
RehamanSyed <rehaman.syed1990@gmail.com>
2023-06-18 23:25:53 +0300

    update ui in All components

--- src/components/Reactjs.js
+++ src/components/Reactjs.js
@@ -30,103 +30,101 @@ const Reactjs = () => {
   if (error) return "An error has occurred: " + error.message;
 
   return (
-    <Box mt={-28} bg={'white'} p={10} rounded={'xl'} shadow={'2xl'} shadowColor>
-
-    <Accordion allowMultiple>
-      {data &&
-        data.map((item, idx) => {
-          return (
-            <AccordionItem border="none" key={idx} mb={2}>
-              <AccordionButton
-                bg={"gray.100"}
-                rounded="2xl"
-                _expanded={{ bg: "teal.500", color: "white", rounded: "2xl" }}
-              >
-                <Box
-                  as="span"
-                  flex="1"
-                  textAlign="left"
-                  fontWeight="medium"
-                  fontSize={16}
-                  py={2}
-                  px={5}
+    <Box mt={-28} bg={"white"} p={10} rounded={"xl"} shadow={"2xl"}>
+      <Accordion allowMultiple>
+        {data &&
+          data.map((item, idx) => {
+            return (
+              <AccordionItem border="none" key={idx} mb={2}>
+                <AccordionButton
+                  bg={"gray.100"}
+                  rounded="2xl"
+                  _expanded={{ bg: "teal.500", color: "white", rounded: "2xl" }}
                 >
-                  {idx + 1}. {item.question}
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-              <AccordionPanel p={5}>
-                <Box
-                  dangerouslySetInnerHTML={{ __html: item.answer }}
-                  sx={{
-                    "& div": {
-                      color: "black",
-                      fontSize: 14,
-                    },
-                    "& strong": {
-                      color: "red.500",
-                      fontWeight: "semibold",
-                    },
-                    "& p": {
-                      color: "black.500",
-                      fontSize: 14,
-                    },
-                    "& code": {
-                      backgroundColor: "red.600",
-                      padding: "0.1rem",
-                      borderRadius: "0.2rem",
-                    },
-                  }}
-                ></Box>
-                {item.example === null ? (
-                  ""
-                ) : (
-                  <Button
-                    as="a"
-                    target="_blank"
-                    variant={"link"}
-                    fontWeight={"light"}
-                    colorScheme={"blue"}
-                    fontSize={14}
-                    font
-                    mt={5}
-                    href={item.example}
+                  <Box
+                    as="span"
+                    flex="1"
+                    textAlign="left"
+                    fontWeight="medium"
+                    fontSize={16}
+                    py={2}
+                    px={5}
                   >
-                    See the Example
-                  </Button>
-                )}
-              </AccordionPanel>
-            </AccordionItem>
-          );
-        })}
-      {data && data.length == 0 && (
-        <Container
-          centerContent
-          bg={"gray.50"}
-          maxW={"container"}
-          rounded={"3xl"}
-        >
-          <Flex
-            minH={"50vh"}
-            flexDir={"column"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            gap={5}
+                    {idx + 1}. {item.question}
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel p={5}>
+                  <Box
+                    dangerouslySetInnerHTML={{ __html: item.answer }}
+                    sx={{
+                      "& div": {
+                        color: "black",
+                        fontSize: 14,
+                      },
+                      "& strong": {
+                        color: "red.500",
+                        fontWeight: "semibold",
+                      },
+                      "& p": {
+                        color: "black.500",
+                        fontSize: 14,
+                      },
+                      "& code": {
+                        backgroundColor: "red.600",
+                        padding: "0.1rem",
+                        borderRadius: "0.2rem",
+                      },
+                    }}
+                  ></Box>
+                  {item.example === null ? (
+                    ""
+                  ) : (
+                    <Button
+                      as="a"
+                      target="_blank"
+                      variant={"link"}
+                      fontWeight={"light"}
+                      colorScheme={"blue"}
+                      fontSize={14}
+                      mt={5}
+                      href={item.example}
+                    >
+                      See the Example
+                    </Button>
+                  )}
+                </AccordionPanel>
+              </AccordionItem>
+            );
+          })}
+        {data && data.length == 0 && (
+          <Container
+            centerContent
+            bg={"gray.50"}
+            maxW={"container"}
+            rounded={"3xl"}
           >
-            <Image
-              src="./empty.png"
-              width="100"
-              height="100"
-              filter="grayscale(50%)"
-              alt="no-image"
-            />
-            <Heading fontWeight={"light"} fontSize={20}>
-              No Data available
-            </Heading>
-          </Flex>
-        </Container>
-      )}
-    </Accordion>
+            <Flex
+              minH={"50vh"}
+              flexDir={"column"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              gap={5}
+            >
+              <Image
+                src="./empty.png"
+                width="100"
+                height="100"
+                filter="grayscale(50%)"
+                alt="no-image"
+              />
+              <Heading fontWeight={"light"} fontSize={20}>
+                No Data available
+              </Heading>
+            </Flex>
+          </Container>
+        )}
+      </Accordion>
     </Box>
   );
 };
